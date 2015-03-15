83485fd92998dd42141768bc659ce77db90a3edb
Pierre Karashchuk <krchtchk@gmail.com>
2015-03-15 00:13:41 -0700

    clean up code

--- index.js
+++ index.js
@@ -73,9 +73,6 @@ function getDirections(lat, lon, callback) {
 	lat + ',' + lon,//origin
 	'37.866441,-122.265410',//destination
         callback,
-	// function(err, data){ 	//callback
-	//     return parseResponse(data);
-	// },						
 	null,					//sensor
 	'walking',				//mode
 	null,					//waypoints
